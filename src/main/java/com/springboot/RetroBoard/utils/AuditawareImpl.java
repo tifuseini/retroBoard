4e2e2065f459991807997d9fd881d1c0d6df18c9
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 23:10:34 +0000

    Implementing Auditor Aware

--- src/main/java/com/springboot/RetroBoard/utils/AuditawareImpl.java
+++ src/main/java/com/springboot/RetroBoard/utils/AuditawareImpl.java
@@ -1,4 +1,17 @@
 package com.springboot.RetroBoard.utils;
 
-public class AuditawareImpl {
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+
+import java.util.Optional;
+
+@Component
+public class AuditawareImpl implements AuditorAware {
+
+
+
 }
