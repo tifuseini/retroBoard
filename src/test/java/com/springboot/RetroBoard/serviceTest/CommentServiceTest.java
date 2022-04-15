7764def432b86359e9e1722a91384d1e3a750566
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 19:43:35 +0000

    MockBean

--- src/test/java/com/springboot/RetroBoard/serviceTest/CommentServiceTest.java
+++ src/test/java/com/springboot/RetroBoard/serviceTest/CommentServiceTest.java
@@ -1,4 +1,29 @@
 package com.springboot.RetroBoard.serviceTest;
 
+import com.springboot.RetroBoard.repository.CommentRepository;
+import com.springboot.RetroBoard.service.CommentService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class CommentServiceTest {
+
+    @MockBean
+    private CommentRepository commentRepository;
+
+    private CommentService commentService;
+
+    @Before
+    public void init(){
+        commentService =new CommentService(commentRepository);
+    }
+
+    @Test
+    public void getAllComment4Today_Path_ShouldReturn1Comment(){
+
+        //Giveb
+    }
 }
