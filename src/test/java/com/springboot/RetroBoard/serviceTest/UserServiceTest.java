d8522fadefb37d4260e605bea618a774699bee65
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 20:37:49 +0000

    User service Test Update

--- src/test/java/com/springboot/RetroBoard/serviceTest/UserServiceTest.java
+++ src/test/java/com/springboot/RetroBoard/serviceTest/UserServiceTest.java
@@ -8,7 +8,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTest {
@@ -20,7 +23,7 @@ public class UserServiceTest {
 
     @Before
     public void init(){
-        this.userService = userService;
+        this.userService = new UserService(userReposiitory);
     }
 
     @Test
@@ -31,5 +34,13 @@ public class UserServiceTest {
         user.setUsername("tifuseini");
         user.setPassword("root");
         user.setRole("User");
+
+        when(userReposiitory.findByUsername("tifuseini")).thenReturn(user);
+
+        //When
+        UserDetails actual = userService.loadUserByUsername("tifuseini");
+
+        //Then
+        verify(userReposiitory,times(1)).findByUsername("tifuseini");
     }
 }
