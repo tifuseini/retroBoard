61e58df3813b3bed7b29d1b527242b3841d5fd99
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 20:25:32 +0000

    @mockBean and @Before

--- src/test/java/com/springboot/RetroBoard/serviceTest/UserServiceTest.java
+++ src/test/java/com/springboot/RetroBoard/serviceTest/UserServiceTest.java
@@ -1,4 +1,23 @@
 package com.springboot.RetroBoard.serviceTest;
 
+
+import com.springboot.RetroBoard.repository.UserReposiitory;
+import com.springboot.RetroBoard.service.UserService;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class UserServiceTest {
+
+    @MockBean
+    private UserReposiitory userReposiitory;
+
+    private UserService userService;
+
+    @Before
+    public void init(){
+        this.userService = userService;
+    }
 }
