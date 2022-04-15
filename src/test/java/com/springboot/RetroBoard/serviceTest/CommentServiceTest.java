ec1852ed729167fc768c175a7ecd17941fc1257b
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 20:19:17 +0000

    comment Service Test 3

--- src/test/java/com/springboot/RetroBoard/serviceTest/CommentServiceTest.java
+++ src/test/java/com/springboot/RetroBoard/serviceTest/CommentServiceTest.java
@@ -76,5 +76,33 @@ public class CommentServiceTest {
         verify(commentRepository,times(1)).saveAll(comments);
     }
 
+    @Test
+    public void saveAll_path_shouldSave2Comments(){
+
+        //Given
+        Comment comment = new Comment();
+        comment.setComment("test 1");
+        comment.setType(CommentType.DELTA);
+        comment.setCreatedBy("Fuseini");
+        comment.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+
+
+        Comment comment2 = new Comment();
+        comment2.setComment("test 2");
+        comment2.setType(CommentType.DELTA);
+        comment2.setCreatedBy("Iddrisu");
+        comment2.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+
+        List<Comment>comments = Arrays.asList(comment,comment2);
+        when(commentRepository.saveAll(comments)).thenReturn(comments);
+
+        //When
+        List<Comment> saved = commentService.saveAll(comments);
+
+        //Then
+        assertThat(saved).isNotEmpty();
+        verify(commentRepository,times(1)).saveAll(comments);
+    }
+
 
 }
