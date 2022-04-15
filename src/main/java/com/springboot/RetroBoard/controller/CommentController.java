4b5440165b06ec5be866dcfba44ee0d02a879900
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 21:32:15 +0000

    Get Mapping

--- src/main/java/com/springboot/RetroBoard/controller/CommentController.java
+++ src/main/java/com/springboot/RetroBoard/controller/CommentController.java
@@ -1,4 +1,45 @@
 package com.springboot.RetroBoard.controller;
 
+import com.springboot.RetroBoard.model.Comment;
+import com.springboot.RetroBoard.model.CommentType;
+import com.springboot.RetroBoard.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
 public class CommentController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        List<Comment> allComments = commentService.getAllCommentsForToday();
+        Map<CommentType, List<Comment>> groupedComments = allComments.stream().collect(Collectors.groupingBy(Comment::getType));
+        model.addAttribute("starComments", groupedComments.get(CommentType.STAR));
+        model.addAttribute("deltaComments", groupedComments.get(CommentType.DELTA));
+        model.addAttribute("plusComments", groupedComments.get(CommentType.PLUS));
+
+        return "comment";
+    }
+
+
 }
