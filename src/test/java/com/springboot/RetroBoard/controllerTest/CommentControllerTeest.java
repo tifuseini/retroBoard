dc6f2e69bc4a0a39774d1d9274b14110c85a9915
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 22:00:25 +0000

    Comment Controlle Test Update

--- src/test/java/com/springboot/RetroBoard/controllerTest/CommentControllerTeest.java
+++ src/test/java/com/springboot/RetroBoard/controllerTest/CommentControllerTeest.java
@@ -1,4 +1,23 @@
 package com.springboot.RetroBoard.controllerTest;
 
+import com.springboot.RetroBoard.controller.CommentController;
+import com.springboot.RetroBoard.service.CommentService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CommentController.class)
 public class CommentControllerTeest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CommentService commentService;
+
+
 }
