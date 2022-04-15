916f660827e28009c12478060f9404b8e611289e
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-15 19:37:12 +0000

    User Service

--- src/main/java/com/springboot/RetroBoard/service/UserService.java
+++ src/main/java/com/springboot/RetroBoard/service/UserService.java
@@ -1,4 +1,42 @@
 package com.springboot.RetroBoard.service;
 
-public class UserService {
+import com.springboot.RetroBoard.model.User;
+import com.springboot.RetroBoard.repository.UserReposiitory;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService implements UserDetailsService {
+
+    private final UserReposiitory userReposiitory;
+
+
+
+    public UserService(UserReposiitory userReposiitory){
+        this.userReposiitory = userReposiitory;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws  UsernameNotFoundException{
+        User user = userReposiitory.findByUsername(username);
+
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
+
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public User create(User user) {
+        return userReposiitory.save(user);
+    }
 }
