5176532bbbf0368e63ea1c0cce0db4046ea3e378
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-16 07:36:47 +0000

    Checking if RestController would work

--- src/main/java/com/springboot/RetroBoard/controller/CommentController.java
+++ src/main/java/com/springboot/RetroBoard/controller/CommentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
